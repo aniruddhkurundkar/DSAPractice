8eb185d8f0285fcd5025dc59a561d4ccd7ec2b43
aniruddhkurundkar10 <aniruddhkurundkar10@gmail.com>
2023-09-12 00:08:57 +0530

    Created new class to find min and max element from the given array.

---
+++ src/com.practice.arrays/FindMaxAndMinOfArray.java
@@ -0,0 +1,34 @@
+package com.practice.arrays;
+
+import java.util.Scanner;
+
+public class FindMaxAndMinOfArray {
+
+    public static void main(String[] str){
+        Scanner scan = new Scanner(System.in);
+        int n = 0;
+        System.out.print("Enter the size of array : ");
+        n=scan.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter the array element : ");
+        for(int i=0;i<n;i++){
+            arr[i] = scan.nextInt();
+        }
+        FindMaxAndMinOfArray mm = new FindMaxAndMinOfArray();
+        mm.findMaxAndMin(arr);
+    }
+
+    public void findMaxAndMin(int[] array){
+        int max = 0;
+        int min = array[0];
+        for(int i=0;i<array.length;i++){
+            if(array[i] > max){
+                max = array[i];
+            }
+            if(array[i] < min){
+                min = array[i];
+            }
+        }
+        System.out.print("Max : "+ max + " Min : " + min);
+    }
+}
