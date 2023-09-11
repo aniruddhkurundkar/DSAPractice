3292f04f068cb7119c16db0a827f63b6123199f4
aniruddhkurundkar10 <aniruddhkurundkar10@gmail.com>
2023-09-11 21:59:21 +0530

    Created new class to rotate array in clockwise direction.

---
+++ src/com.practice.arrays/RotateArrayInClockWise.java
@@ -0,0 +1,80 @@
+package com.practice.arrays;
+
+/**
+ * Given an array of N element, rotate it in clockwise direction by k times.
+ * for example:
+ * Input : int[] array = {3,-2,1,4,6,9,8}
+ * Output: array = {4,6,9,8,3,-2,1}
+ */
+
+import java.util.Scanner;
+
+/**
+ * OBSERVATION:
+ * We can get the rotated array by using reverse array. They are same but only the order differs.
+ * 1. Reverse the entire array. Iteration could n/2 times.
+ * 2. Reverse first k elements of an array with iteration k/2 times.
+ * 3. Reverse last n-k elements of an array with (n-k)/2 iteration.
+ *
+ * TC = BigO(n) and SC = BigO(1)
+ */
+public class RotateArrayInClockWise {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = 0;
+        System.out.print("Enter the size of an array : ");
+        n = scan.nextInt();
+        int[] A = new int[n];
+        System.out.print("Enter the elements of an array : ");
+        for(int i=0;i<n;i++){
+            A[i]=scan.nextInt();
+        }
+        int k=0;
+        System.out.print("Enter the value of K : ");
+        k = scan.nextInt();
+        RotateArrayInClockWise rotate = new RotateArrayInClockWise();
+        rotate.reverseEntireArray(A,n);
+        rotate.reverseFirstKElements(A,k);
+        rotate.reverseLastElements(A,k,n-1);
+    }
+
+    public int[] reverseEntireArray(int[] A, int n){
+        int p=0;
+        int q=n-1;
+        int temp = 0;
+        while(p<q){
+            temp = A[p];
+            A[p] = A[q];
+            A[q] = temp;
+            p++;
+            q--;
+        }
+        return A;
+    }
+    public int[] reverseFirstKElements(int[] A, int k){
+        int p=0;
+        int q=k-1;
+        int temp = 0;
+        while(p<q){
+            temp = A[p];
+            A[p] = A[q];
+            A[q] = temp;
+            p++;
+            q--;
+        }
+        return A;
+    }
+    public int[] reverseLastElements(int[] A,int k, int m){
+        int p=k;
+        int q=m;
+        int temp = 0;
+        while(p<q){
+            temp = A[p];
+            A[p] = A[q];
+            A[q] = temp;
+            p++;
+            q--;
+        }
+        return A;
+    }
+}
